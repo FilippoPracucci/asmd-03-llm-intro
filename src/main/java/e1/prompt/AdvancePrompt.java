41bf22ab65aa74838c8fc4431c56b7e545d0b37a
Filippo Pracucci <filippo.pracucci@studio.unibo.it>
2026-03-06 12:57:04 +0100

    feat: first solution to exercise 1 (StoryPrompt)

--- src/main/java/e1/prompt/AdvancePrompt.java
+++ src/main/java/e1/prompt/AdvancePrompt.java
@@ -8,10 +8,27 @@ public record AdvancePrompt(
     String chosenAction
 ) implements StoryPrompt {
 
-    private static final String TEMPLATE = "TODO";
+    private static final String TEMPLATE = """
+            You are the narrator of an text-adventure game, so you have to ask the player question then he/she will make
+            a choice.
+            You have to continue a story considering the player stats,
+            the previous question and the chosen action by the player.
+            The player called %s has %d level of health and %d of attack power.
+            The previous question is: %s.
+            The action chosen by the player is: %s
+            Respond with ONLY a JSON object: { "narrative": String, "question": String, "choices": [String],
+            "updatedPlayer": { "name": String, "health": Number, "attack": Number }, "gameOver": Boolean }.
+            No explanation
+            """;
 
     @Override
     public String toPromptString() {
-        throw new UnsupportedOperationException("Not supported yet.");
+        return String.format(TEMPLATE,
+                player.name(),
+                player.health(),
+                player.attackPower(),
+                previousQuestion,
+                chosenAction
+        );
     }
 }
--- src/main/java/e1/prompt/BeginPrompt.java
+++ src/main/java/e1/prompt/BeginPrompt.java
@@ -3,10 +3,24 @@ package e1.prompt;
 import e1.model.Player;
 
 public record BeginPrompt(Player player, String setting) implements StoryPrompt {
-    private static final String TEMPLATE = "TODO";
+    private static final String TEMPLATE = """
+            You are the narrator of an text-adventure game, so you have to ask the player question then he/she will make
+            a choice.
+            You have to start a new story considering the player stats and settings.
+            The player called %s has %d level of health and %d of attack power.
+            The setting description represents the context of the story and is: %s.
+            Respond with ONLY a JSON object: { "narrative": String, "question": String, "choices": [String],
+            "updatedPlayer": { "name": String, "health": Number, "attack": Number }, "gameOver": Boolean }.
+            No explanation.
+            """;
 
     @Override
     public String toPromptString() {
-        throw new UnsupportedOperationException("Not supported yet.");
+        return String.format(TEMPLATE,
+                player.name(),
+                player.health(),
+                player.attackPower(),
+                setting
+        );
     }
 }
